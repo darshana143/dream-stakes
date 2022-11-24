946f67cb80b31b1221a75fe8848db5c0f3c8ac57
darshana143 <darshana@siliconlogics.com>
2022-11-25 00:14:37 +0530

    top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopBarComponent } from './ui-containers/top-bar/top-bar.component';
 import { LeftPanalComponent } from './ui-containers/left-panal/left-panal.component';
+
+// -------------------------------- PrimeNG ----------------------------------------
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { LeftPanalComponent } from './ui-containers/left-panal/left-panal.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
